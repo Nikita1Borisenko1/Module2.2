8dcdf1c097b03e3c11b723052fee4731951c96cb
ZKDS <nikitoc0@mail.ru>
2024-03-12 10:25:19 +0300

    111

--- Реализации подсчета всех баллов/Program.cs
+++ Реализации подсчета всех баллов/Program.cs
@@ -8,6 +8,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        string fullName = "Борисенко Никита Александрович";
+        byte age = 20;
+        string email = "toc044toc@gmail.com";
         int programmingscores = 40;
         int mathscores = 70;
         int physicsscores = 69;
@@ -15,8 +18,14 @@ class Program
         int b = (int)a / 3; // средний балл
         double c =  a / 3; // среднее арифметическое
         string cFormated = c.ToString("#.#"); // ограничение чисел после запятой
-        string pattern = "Сумма баллов: {0} \nсредний балл: {1} \nсреднее арифметическое баллов: {2}";
+        string pattern = "Ф.И.О.: {0} \nВозраст: {1} \nЭл.Почта: {2} \nБаллы по программированию: {3} \nБаллы по математике: {4} \nБаллы по физике: {5} \nСумма баллов: {6} \nСредний балл: {7} \nСреднее арифметическое баллов: {8}";
         Console.WriteLine(pattern,
+            fullName,
+            age,
+            email,
+            programmingscores,
+            mathscores,
+            physicsscores,
              a,
              b,
              cFormated);
